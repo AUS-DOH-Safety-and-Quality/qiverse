ed027d728e64834196e697b53ef2c900a6928889
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-12-16 13:21:56 +0800

    Add utility function for listing all dataflows (not exposed)

--- R/workspaces.R
+++ R/workspaces.R
@@ -24,3 +24,27 @@ list_workspaces <- function(access_token) {
   names(content_to_dataframe) <- c("Workspace", "WorkspaceId", "CapacityID")
   content_to_dataframe
 }
+
+
+list_dataflows <- function(workspace_id, access_token) {
+  base_url <- paste0("https://api.powerbi.com/v1.0/myorg/groups/", workspace_id, "/dataflows")
+  metadata_request <- httr::GET(url = base_url,
+                                config = get_auth_header(access_token),
+                                httr::content_type_json())
+
+  if (metadata_request$status_code != 200) {
+    stop("API request returned status code: ", metadata_request$status_code, "!",
+         call. = TRUE)
+  }
+
+  metadata_content <- httr::content(metadata_request)
+
+  content_to_dataframe <- metadata_content$value |>
+    purrr::keep(\(x) !is.null(x$name)) |>
+    purrr::map_dfr(\(x) {
+      x[c("name", "objectId")]
+    })
+
+  names(content_to_dataframe) <- c("Dataflow", "DataflowId")
+  content_to_dataframe
+}
